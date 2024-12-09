daf0f1df2ead7728e5d4982f6624d60595cdce64
Manish <man112056@gmail.com>
2024-12-09 17:16:38 +0530

    added cfg file and checked loading in main

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,12 +1,17 @@
 package com.example;
 
-/**
- * Hello world!
- *
- */
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
 public class App{
-    public static void main( String[] args )
-    {
+    public static void main( String[] args ) {
         System.out.println( "Hello World!" );
+
+        Configuration configuration = new Configuration();
+        configuration.configure();// can be defined hibernate.cfg.xml file manually
+
+        SessionFactory factory = configuration.buildSessionFactory();
+        System.out.println(factory);
+        System.out.println(factory.isOpen());
     }
 }
